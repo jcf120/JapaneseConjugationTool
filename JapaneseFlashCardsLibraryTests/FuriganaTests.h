d4f3d8e5a678cb2fc9f8a78fefce55b2c64217a2
Joss Clifford-Frith <jossie.cliffordfrith@gmail.com>
2013-03-14 18:45:58 +0900

    Moved to correct directory

---
+++ JapaneseFlashCardsLibraryTests/FuriganaTests.h
@@ -0,0 +1,16 @@
+//
+//  FuriganaTests.h
+//  JapaneseFlashCards
+//
+//  Created by Jocelyn Clifford-Frith on 19/09/2012.
+//  Copyright (c) 2012 Jocelyn Clifford-Frith. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "Furigana.h"
+
+@interface FuriganaTests : SenTestCase
+
++ (NSArray *)validMarkedStrings;
+
+@end
